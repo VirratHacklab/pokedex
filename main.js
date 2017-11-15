441c3b14f7d5c7d1760bc901b819fea538a91fef
Antti Rintala <antti@rintala.it>
2017-11-15 13:57:05 +0200

    Upgrade

--- main.js
+++ main.js
@@ -1,39 +1,74 @@
-var app = require('app');  // Module to control application life.
-var BrowserWindow = require('browser-window');  // Module to create native browser window.
+const electron = require('electron');
+const path = require('path');
 
-// Report crashes to our server.
-require('crash-reporter').start();
+const { app, BrowserWindow } = electron;
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-var mainWindow = null;
+// simple parameters initialization
+const electronConfig = {
+    URL_LAUNCHER_TOUCH: process.env.URL_LAUNCHER_TOUCH === '1' ? 1 : 0,
+    URL_LAUNCHER_TOUCH_SIMULATE: process.env.URL_LAUNCHER_TOUCH_SIMULATE === '1' ? 1 : 0,
+    URL_LAUNCHER_FRAME: process.env.URL_LAUNCHER_FRAME === '1' ? 1 : 0,
+    URL_LAUNCHER_KIOSK: process.env.URL_LAUNCHER_KIOSK === '1' ? 1 : 0,
+    URL_LAUNCHER_NODE: process.env.URL_LAUNCHER_NODE === '1' ? 1 : 0,
+    URL_LAUNCHER_WIDTH: parseInt(process.env.URL_LAUNCHER_WIDTH || 800, 10),
+    URL_LAUNCHER_HEIGHT: parseInt(process.env.URL_LAUNCHER_HEIGHT || 480, 10),
+    URL_LAUNCHER_TITLE: process.env.URL_LAUNCHER_TITLE || 'RESIN.IO',
+    URL_LAUNCHER_CONSOLE: process.env.URL_LAUNCHER_CONSOLE === '1' ? 1 : 0,
+    URL_LAUNCHER_URL: process.env.URL_LAUNCHER_URL || `file:///${path.join(__dirname, 'app', 'index.html')}`,
+    URL_LAUNCHER_ZOOM: parseFloat(process.env.URL_LAUNCHER_ZOOM || 1.0),
+    URL_LAUNCHER_OVERLAY_SCROLLBARS: process.env.URL_LAUNCHER_CONSOLE === '1' ? 1 : 0,
+};
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform != 'darwin') {
-        app.quit();
-    }
+// enable touch events if your device supports them
+if (electronConfig.URL_LAUNCHER_TOUCH) {
+    app.commandLine.appendSwitch('--touch-devices');
+}
+// simulate touch events - might be useful for touchscreen with partial driver support
+if (electronConfig.URL_LAUNCHER_TOUCH_SIMULATE) {
+    app.commandLine.appendSwitch('--simulate-touch-screen-with-mouse');
+}
+
+if (process.env.NODE_ENV === 'development') {
+    console.log('Running in development mode');
+    Object.assign(electronConfig, {
+        URL_LAUNCHER_HEIGHT: 480,
+        URL_LAUNCHER_WIDTH: 800,
+        URL_LAUNCHER_KIOSK: 0,
+        URL_LAUNCHER_CONSOLE: 0,
+        URL_LAUNCHER_FRAME: 1,
+    });
+}
+
+/*
+ we initialize our application display as a callback of the electronJS "ready" event
+ */
+app.on('ready', () => {
+    // here we actually configure the behavour of electronJS
+    const window = new BrowserWindow({
+        width: electronConfig.URL_LAUNCHER_WIDTH,
+        height: electronConfig.URL_LAUNCHER_HEIGHT,
+        frame: !!(electronConfig.URL_LAUNCHER_FRAME),
+        title: electronConfig.URL_LAUNCHER_TITLE,
+        kiosk: !!(electronConfig.URL_LAUNCHER_KIOSK),
+        webPreferences: {
+            nodeIntegration: !!(electronConfig.URL_LAUNCHER_NODE),
+            zoomFactor: electronConfig.URL_LAUNCHER_ZOOM,
+            overlayScrollbars: !!(electronConfig.URL_LAUNCHER_OVERLAY_SCROLLBARS),
+        },
+    });
+
+window.webContents.on('did-finish-load', () => {
+    setTimeout(() => {
+    window.show();
+}, 300);
 });
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-app.on('ready', function () {
-    // Create the browser window.
-    mainWindow = new BrowserWindow({width: 800, height: 480});
-    mainWindow.setFullScreen(true);
-    // and load the index.html of the app.
-    mainWindow.loadURL('file://' + __dirname + '/app/index.html');
+// if the env-var is set to true,
+// a portion of the screen will be dedicated to the chrome-dev-tools
+if (electronConfig.URL_LAUNCHER_CONSOLE) {
+    window.openDevTools();
+}
 
-    // Open the DevTools.
-    //mainWindow.webContents.openDevTools();
-
-    // Emitted when the window is closed.
-    mainWindow.on('closed', function () {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        mainWindow = null;
-    });
+// the big red button, here we go
+window.loadURL(electronConfig.URL_LAUNCHER_URL);
 });
